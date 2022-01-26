813ea7d1b9373f09c14776096532b80296160526
virza805 <virza805@gmail.com>
2022-01-27 00:36:19 +0600

    remove updated widgets style

---
+++ child_theme_functions.php
@@ -0,0 +1,9 @@
+<?php  
+
+// remove updated widgets style
+function example_theme_support() {
+    remove_theme_support( 'widgets-block-editor' );
+}
+add_action( 'after_setup_theme', 'example_theme_support' );
+
+
