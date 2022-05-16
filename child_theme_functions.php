5b442e9cc5cc49a1ac3855a7117fcd55e79e08ed
MD AL AMIN <52990610+1mdalamin1@users.noreply.github.com>
2022-05-16 08:50:30 +0600

    Update child_theme_functions.php
    user for old widgets panel & elementor-pro remove_update_notifications

--- child_theme_functions.php
+++ child_theme_functions.php
@@ -6,4 +6,16 @@ function example_theme_support() {
 }
 add_action( 'after_setup_theme', 'example_theme_support' );
 
-add_filter('use_block_editor_for_post', '__return_false', 10);
+add_filter('use_block_editor_for_post', '__return_false', 10); // user old post editor
+add_filter( 'use_widgets_block_editor', '__return_false' ); // user for old widgets panel
+
+
+add_filter( 'site_transient_update_plugins', 'remove_update_notifications' );
+function remove_update_notifications( $value ) {
+
+    if ( isset( $value ) && is_object( $value ) ) {
+        unset( $value->response[ 'elementor-pro/elementor-pro.php' ] );
+    }
+
+    return $value;
+}
