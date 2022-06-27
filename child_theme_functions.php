4bfc08d6e7fa8045626c45809eaac69d62a411e7
MD AL AMIN <52990610+1mdalamin1@users.noreply.github.com>
2022-06-27 16:27:46 +0600

    Add text slider

--- child_theme_functions.php
+++ child_theme_functions.php
@@ -19,3 +19,38 @@ function remove_update_notifications( $value ) {
 
     return $value;
 }
+
+
+
+
+
+
+// add code for Text Slider now 
+
+add_action('wp_head', 'get_custom_script_init');
+function get_custom_script_init(){
+	?>
+    	<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+    	<script>
+			jQuery(document).ready(function () {
+				document.querySelector('.hero_title h2 span').textContent = '';
+
+				var typed = new Typed('.hero_title h2 span', {
+					strings: ["Be Smart","Be Safe","Be Connected", "SEO", "PPC", "Web Design", "Social Media", "Content"],
+					typeSpeed: 70,
+					startDelay: 0,
+					backSpeed: 60,
+					backDelay: 1400,
+					loop: true,
+					showCursor: true,
+					cursorChar: '',
+					autoInsertCss: false
+				});
+
+				//mobile
+			});
+		</script>
+    <?php
+}
+
+
